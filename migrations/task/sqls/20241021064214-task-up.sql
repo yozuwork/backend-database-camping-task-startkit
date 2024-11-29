858def675a00b2ee9b6034f254ab4710febbde90
yozu <yozuwork@gmail.com>
2024-11-29 11:05:01 +0800

    完成答題 - 1. 用戶資料，資料表為 USER

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -15,17 +15,48 @@
 --     5. 用戶名稱為`Q太郎`，Email 為`starplatinum@hexschooltest.io`，Role為`USER`
 --     6. 用戶名稱為 透明人，Email 為 opacity0@hexschooltest.io，Role 為 USER
 
+
+insert  into "USER" ("name","email","role")
+values('李燕容','lee2000@hexschooltest.io','USER');
+
+insert  into "USER" ("name","email","role")
+values('王小明','wXlTq@hexschooltest.io','USER');
+
+insert  into "USER" ("name","email","role")
+values('肌肉棒子','muscle@hexschooltest.io','USER');
+
+insert  into "USER" ("name","email","role")
+values('好野人','richman@hexschooltest.io','USER');
+
+insert  into "USER" ("name","email","role")
+values('Q太郎','starplatinum@hexschooltest.io','USER');
+
+insert  into "USER" ("name","email","role")
+values('透明人','opcatiy0@hexschooltest.io','USER');
+
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 
-
-
+update "USER"
+set "role" = 'COACH'
+where "email" in ('lee2000@hexschooltest.io'
+,'muscle@hexschooltest.io','starplatinum@hexschooltest.io');
 
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
 
+delete  from  "USER"
+where  "email" =  'opcatiy0@hexschooltest.io';
 
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
 
+select  count(*) as "所有用戶數量"
+from "USER";
+
+
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
+
+select  * 
+from "USER" 
+limit 3;
 
 
 --  ████████  █████   █    ████  
