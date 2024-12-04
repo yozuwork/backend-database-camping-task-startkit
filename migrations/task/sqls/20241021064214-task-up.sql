6e3055a097702f478b7e411ff3a66582fb8cb688
yozuwork <yozuwork@gmail.com>
2024-12-04 22:34:52 +0800

    第四大題完成

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -203,7 +203,16 @@ where  name = '空中瑜伽';
     -- 5. 授課結束時間`end_at`設定為2024-11-25 16:00:00
     -- 6. 最大授課人數`max_participants` 設定為10
     -- 7. 授課連結設定`meeting_url`為 https://test-meeting.test.io
-
+insert  into  "COURSE"  (user_id,skill_id,name,start_at,end_at,max_participants,meeting_url)
+values (
+	(select user_id  from "COACH" where  user_id = (select id from "USER" where email = 'lee2000@hexschooltest.io')),
+	(select id from "SKILL" where  name = '重訓'),
+	'重訓基礎課',
+	'2024-11-25 14:00:00',
+	'2024-11-25 16:00:00',
+	10,
+	'https://test-meeting.test.io'
+);
 
 -- ████████  █████   █    █████ 
 --   █ █   ██    █  █     █     
