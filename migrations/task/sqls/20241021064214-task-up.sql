5400aed716b11e0b7300f4e783fcb147f27d8a3c
yozuwork <yozuwork@gmail.com>
2024-12-08 21:00:20 +0800

    新增課程組合包方案及購買紀錄，包含 7、14、21 堂組合包方案的資料插入

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -63,3 +63,47 @@ select  *  from  "USER"  limit 3 ;
 --    █ █   █    ██  █     █     
 --    █ █   █████ █   █    █████ 
 -- ===================== ====================
+-- 2. 組合包方案 CREDIT_PACKAGE、客戶購買課程堂數 CREDIT_PURCHASE
+-- 2-1. 新增：在`CREDIT_PACKAGE` 資料表新增三筆資料，資料需求如下：
+    -- 1. 名稱為 `7 堂組合包方案`，價格為`1,400` 元，堂數為`7`
+    -- 2. 名稱為`14 堂組合包方案`，價格為`2,520` 元，堂數為`14`
+    -- 3. 名稱為 `21 堂組合包方案`，價格為`4,800` 元，堂數為`21`
+
+insert  into  "CREDIT_PACKAGE" (name,credit_amount,price)
+values ('7 堂組合包方案',7,1400);
+
+insert  into  "CREDIT_PACKAGE" (name,credit_amount,price)
+values ('14 堂組合包方案',14,2520);
+
+insert  into  "CREDIT_PACKAGE" (name,credit_amount,price)
+values ('21 堂組合包方案',21,4800);
+
+
+-- 2-2. 新增：在 `CREDIT_PURCHASE` 資料表，新增三筆資料：（請使用 name 欄位做子查詢）
+    -- 1. `王小明` 購買 `14 堂組合包方案`
+    -- 2. `王小明` 購買 `21 堂組合包方案`
+    -- 3. `好野人` 購買 `14 堂組合包方案`
+
+insert  into   "CREDIT_PURCHASE" (user_id,credit_package_id,purchased_credits,price_paid)
+values(
+	(select id from "USER" where email = 'wXlTq@hexschooltest.io'),
+	(select id from "CREDIT_PACKAGE" where name = '14 堂組合包方案'),
+	(select credit_amount from "CREDIT_PACKAGE" where name = '14 堂組合包方案'  ),
+	(select price from "CREDIT_PACKAGE" where name = '14 堂組合包方案' )	);
+	
+
+insert  into   "CREDIT_PURCHASE" (user_id,credit_package_id,purchased_credits,price_paid)
+values(
+	(select id from "USER" where email = 'wXlTq@hexschooltest.io'),
+	(select id from "CREDIT_PACKAGE" where name = '21 堂組合包方案'),
+	(select credit_amount from "CREDIT_PACKAGE" where name = '21 堂組合包方案'  ),
+	(select price from "CREDIT_PACKAGE" where name = '21 堂組合包方案' )	);
+	
+
+insert  into   "CREDIT_PURCHASE" (user_id,credit_package_id,purchased_credits,price_paid)
+values(
+	(select id from "USER" where email = 'richman@hexschooltest.io'),
+	(select id from "CREDIT_PACKAGE" where name = '14 堂組合包方案'),
+	(select credit_amount from "CREDIT_PACKAGE" where name = '14 堂組合包方案'  ),
+	(select price from "CREDIT_PACKAGE" where name = '14 堂組合包方案' )	);
+
