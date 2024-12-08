7538a3e8bc1b2370e8f24806097058b8c6b8e916
yozuwork <yozuwork@gmail.com>
2024-12-08 21:05:17 +0800

    新增課程「重訓基礎課」，設定教練、專長、時間及授課連結

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -188,3 +188,30 @@ insert  into   "SKILL" (name)  values ('空中瑜伽');
 
 delete from  "SKILL" 
 where  name = '空中瑜伽';
+
+--  ████████  █████   █    █   █ 
+--    █ █   ██    █  █     █   █ 
+--    █ █████ ███ ███      █████ 
+--    █ █   █    ██  █         █ 
+--    █ █   █████ █   █        █ 
+-- ===================== ==================== 
+-- 4. 課程管理 COURSE 、組合包方案 CREDIT_PACKAGE
+
+-- 4-1. 新增：在`COURSE` 新增一門課程，資料需求如下：
+    -- 1. 教練設定為用戶`李燕容` 
+    -- 2. 在課程專長 `skill_id` 上設定為「 `重訓` 」
+    -- 3. 在課程名稱上，設定為「`重訓基礎課`」
+    -- 4. 授課開始時間`start_at`設定為2024-11-25 14:00:00
+    -- 5. 授課結束時間`end_at`設定為2024-11-25 16:00:00
+    -- 6. 最大授課人數`max_participants` 設定為10
+    -- 7. 授課連結設定`meeting_url`為 https://test-meeting.test.io
+insert  into  "COURSE"  (user_id,skill_id,name,start_at,end_at,max_participants,meeting_url)
+values (
+	(select id from "USER" where email = 'lee2000@hexschooltest.io'),
+	(select id from "SKILL" where  name = '重訓'),
+	'重訓基礎課',
+	'2024-11-25 14:00:00',
+	'2024-11-25 16:00:00',
+	10,
+	'https://test-meeting.test.io'
+);
