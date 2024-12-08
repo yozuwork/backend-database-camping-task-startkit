094d8c47adfda248f0fd6a8d22c364f9e2bcbf41
yozuwork <yozuwork@gmail.com>
2024-12-08 21:09:12 +0800

    更新 COURSE_BOOKING 資料，取消王小明的預約並新增再次預約紀錄

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -249,3 +249,24 @@ values (
       '即將授課'
 
     );
+-- 5-2. 修改：`王小明`取消預約 `李燕容` 的課程，請在`COURSE_BOOKING`更新該筆預約資料：
+    -- 1. 取消預約時間`cancelled_at` 設為2024-11-24 17:00:00
+    -- 2. 狀態`status` 設定為課程已取消
+
+    update  "COURSE_BOOKING" 
+    set  cancelled_at  = '2024-11-24 17:00:00',
+    status  = '課程已取消' 
+    where  user_id = (select id from "USER" where email = 'wXlTq@hexschooltest.io');
+
+-- 5-3. 新增：`王小明`再次預約 `李燕容`   的課程，請在`COURSE_BOOKING`新增一筆資料：
+    -- 1. 預約人設為`王小明`
+    -- 2. 預約時間`booking_at` 設為2024-11-24 17:10:25
+    -- 3. 狀態`status` 設定為即將授課
+  insert  into  "COURSE_BOOKING" (user_id,course_id,booking_at,status)
+  values (
+    (select id from  "USER"  where email = 'wXlTq@hexschooltest.io'),
+    (select id from  "COURSE" where  user_id =  (select id from "USER" where email = 'lee2000@hexschooltest.io')  ),
+    '2024-11-24 17:10:25',
+    '即將授課'
+
+  );
