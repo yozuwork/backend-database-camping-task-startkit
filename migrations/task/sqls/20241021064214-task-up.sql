4bb61aab27f26ad0fdd94f141c25eda8c7c1efa1
yozuwork <yozuwork@gmail.com>
2024-12-08 20:41:17 +0800

    為取消和上課中狀態新增逗號

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -252,7 +252,7 @@ values (
     -- 2. 狀態`status` 設定為課程已取消
 
     update  "COURSE_BOOKING" 
-    set  cancelled_at  = '2024-11-24 17:00:00'
+    set  cancelled_at  = '2024-11-24 17:00:00',
     status  = '課程已取消' 
     where  user_id = (select id from "USER" where email = 'wXlTq@hexschooltest.io');
 
@@ -276,7 +276,7 @@ values (
     -- 1. 請在該筆預約記錄他的加入直播室時間 `join_at` 設為2024-11-25 14:01:59
     -- 2. 狀態`status` 設定為上課中
   update  "COURSE_BOOKING" 
-  set  join_at  = '2024-11-25 14:01:59'
+  set  join_at  = '2024-11-25 14:01:59',
   status  = '上課中' 
   where  user_id = (select id from "USER" where email = 'wXlTq@hexschooltest.io'  ) 
   and status  = '即將授課' ;
